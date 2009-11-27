280d28e97227d784747f9904822d6ae000bc6ed2
Steve Lambert <steve@steve-lamberts-macbook.local>
2009-11-27 15:58:46 -0500

    updated index.php and commented out the previous and next buttons until ticket 380 is fixed. http://dev.eyebeam.org/projects/wpfolio/ticket/380

--- index.php
+++ index.php
@@ -3,11 +3,18 @@
 <?php 	if (! empty($display_stats) ) { 		get_stats(1); 		echo "<br />"; 	} 	else if (($posts & empty($display_stats)) ) : foreach ($posts as $post) : the_post(); ?>   
 
 <div class="entry">   
+	
+<!--COMMENTING OUR PREVIOUS/NEXT BUTTONS - they page through multiple categories.
+See ticket http://dev.eyebeam.org/projects/wpfolio/ticket/380
+
 <div class="prevnext"> 
-<!--<?php previous_posts_link('&larr;') ?> <?php next_post('%', '&larr;', 'no'); ?>  <?php previous_post('%', '&rarr;', 'no'); ?> <?php next_posts_link('&rarr;') ?>-->
+?php previous_posts_link('&larr;') ?> <?php next_post('%', '&larr;', 'no'); ?>  <?php previous_post('%', '&rarr;', 'no'); ?> <?php next_posts_link('&rarr;') ?>
 <?php next_post_link('%link', 'newer', TRUE, ''); ?> 
 <?php previous_post_link('%link', 'older', TRUE, ''); ?>
-</div>    
+</div>
+
+END PREVIOUS/NEXT BUTTONS 
+-->    
 
 <?php the_content(); ?>   
 </div> 		    
