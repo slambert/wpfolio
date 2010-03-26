62adc9f5aec12d28f5b52f9203223b4e3aa9d448
slambert <steve@visitsteve.com>
2010-03-26 15:56:05 -0400

    adding line break so edit entry link wont overlay on art info in single post view

--- index.php
+++ index.php
@@ -23,7 +23,8 @@
 
 <div class="posted">   
 <b><?php the_title(); ?></b>  | <a href="<?php the_permalink() ?>" title="Permalink"><?php the_time('Y') ?></a> | <?php the_category(', '); ?> <?php the_tags('| Tags: ',', ',''); ?>  | <?php comments_popup_link(__('Comments (0)'), __('Comments (1)'), __('Comments (%)'), __(''), __('')); ?>
-<?php edit_post_link('edit this entry', '<br /><span class="adminuser">', '</span>'); ?> <!--USER EDIT LINK-->
+
+<?php edit_post_link('edit this entry', '<br /><br /><span class="adminuser">', '</span>'); ?> <!--USER EDIT LINK-->
 </div><!-- .posted -->
      
 <?php comments_template(); ?> 	   
