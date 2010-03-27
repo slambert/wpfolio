7200e1b7eeed5cc58c4dabf9774bd67d97aeeede
danielwiener <daniel@danielwiener.com>
2010-03-27 14:19:05 -0400

    added two divs for left and right sections of footer, and added the two footer sidebars

--- footer.php
+++ footer.php
@@ -3,16 +3,20 @@
 <!-- License information can go here -->
 <!-- Maybe this can be of help - http://creativecommons.org/about -->
 
-<a href="feed:<?php bloginfo('rss2_url'); ?>">Entries (RSS)</a> and <a
-href="feed:<?php bloginfo('comments_rss2_url'); ?>">Comments(RSS)</a>.
+<div class="left">
+	<?php
+	if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('Footer Left') ){
+		/* get_sidebar('footer_left'); */	}
+				?>
+	<!-- <?php echo $wpdb->num_queries; ?> queries. <?php timer_stop(1); ?> seconds. -->
+</div> <!-- end footer left -->
+<div class="right">
+	<?php
+	if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('Footer Right') ){
+		/* get_sidebar('footer_left'); */	}
+				?>
 
-
-
-
-<!-- <?php echo $wpdb->num_queries; ?> queries. <?php timer_stop(1); ?>
-seconds. -->
-
-
+ </div> <!-- end footer right -->
 
 </div><!-- footer -->
 
