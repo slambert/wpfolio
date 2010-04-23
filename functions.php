19e93224fe67c51305b80c0d0e6d6f58274af9b5
slambert <steve@visitsteve.com>
2010-04-22 21:08:42 -0400

    created custom fields for posts. Doesnt appear. Needs love. See ticket 422 for details.

--- functions.php
+++ functions.php
@@ -1,5 +1,187 @@
 <?php 
 
+
+//	ADDS CUSTOM FIELDS for dimensions and media of artwork.
+//  Taken from http://sltaylor.co.uk/blog/control-your-own-wordpress-custom-fields/
+//  Steve Lambert added this on 4/22/10 and he's not sure it works.
+
+if ( !class_exists('WPFolioCustomFields') ) {  
+
+	class WPFolioCustomFields {  
+			/** 
+			* @var  string  $prefix  The prefix for storing custom fields in the postmeta table 
+			*/  	
+			var $prefix = '_wpfcf_';
+
+			/**
+			* @var  array  $customFields  Defines the custom fields available
+			*/
+			var $customFields =	array(
+				array(
+					"name"			=> "media",
+					"title"			=> "Media",
+					"description"	=> "media of the work (i.e. photography, graphite on paper, etc.)",
+					"type"			=> "textarea",
+					"scope"			=>	array( "page" ),
+					"capability"	=> "edit_pages"
+					),
+				array(
+					"name"			=> "dimensions",
+					"title"			=> "Dimensions",
+					"description"	=> "H x W x D",
+					"type"			=> "textarea",
+					"scope"			=>	array( "page" ),
+					"capability"	=> "edit_pages"
+					),
+				array(
+					"name"			=> "collaborators",				
+					"title"			=> "Collaborators",
+					"description"	=> "if solo work, leave blank",
+					"type"			=> "textarea",
+					"scope"			=>	array( "page" ),
+					"capability"	=> "edit_pages"
+					),
+			);
+
+		/**
+		* PHP 4 Compatible Constructor
+		*/
+		function WPFolioCustomFields() { $this->__construct(); }
+		
+		/**
+		* PHP 5 Constructor
+		*/
+		function __construct() {
+			add_action( 'admin_menu', array( &$this, 'createCustomFields' ) );
+			add_action( 'save_post', array( &$this, 'saveCustomFields' ), 1, 2 );
+			// Comment this line out if you want to keep default custom fields meta box
+			add_action( 'do_meta_boxes', array( &$this, 'removeDefaultCustomFields' ), 10, 3 );
+		}
+		/**
+		* Remove the default Custom Fields meta box
+		*/
+		function removeDefaultCustomFields( $type, $context, $post ) {
+			foreach ( array( 'normal', 'advanced', 'side' ) as $context ) {
+				remove_meta_box( 'postcustom', 'post', $context );
+				remove_meta_box( 'postcustom', 'page', $context );
+				//Use the line below instead of the line above for WP versions older than 2.9.1
+				//remove_meta_box( 'pagecustomdiv', 'page', $context );
+			}
+		}
+		
+		/**
+		* Create the new Custom Fields meta box
+		*/
+		function createCustomFields() {
+			if ( function_exists( 'add_meta_box' ) ) {
+				//we don't want custom fields for PAGES, so we're leaving this out.
+				// add_meta_box( 'wpfolio-custom-fields', 'WPFolio Custom Fields', array( &$this, 'displayCustomFields' ), 'page', 'normal', 'high' );
+				add_meta_box( 'wpfolio-custom-fields', 'WPFolio Custom Fields', array( &$this, 'displayCustomFields' ), 'post', 'normal', 'high' );
+			}
+		}
+		
+		/**
+		* Display the new Custom Fields meta box
+		*/
+		function displayCustomFields() {
+			global $post;
+			?>
+			<div class="form-wrap">
+				<?php
+				wp_nonce_field( 'wpfolio-custom-fields', 'wpfolio-custom-fields_wpnonce', false, true );
+				foreach ( $this->customFields as $customField ) {
+					// Check scope
+					$scope = $customField[ 'scope' ];
+					$output = false;
+					foreach ( $scope as $scopeItem ) {
+						switch ( $scopeItem ) {
+							case "post": {
+								// Output on any post screen
+								if ( basename( $_SERVER['SCRIPT_FILENAME'] )=="post-new.php" || $post->post_type=="post" )
+									$output = true;
+								break;
+							}
+							case "page": {
+								// Output on any page screen
+								if ( basename( $_SERVER['SCRIPT_FILENAME'] )=="page-new.php" || $post->post_type=="page" )
+									$output = true;
+								break;
+							}
+						}
+						if ( $output ) break;
+					}
+					
+		// Check capability
+		if ( !current_user_can( $customField['capability'], $post->ID ) )
+			$output = false;
+					// Output if allowed
+					if ( $output ) { ?>
+						<div class="form-field form-required">
+							<?php
+							switch ( $customField[ 'type' ] ) {
+								case "checkbox": {
+									// Checkbox
+									echo '<label for="' . $this->prefix . $customField[ 'name' ] .'" style="display:inline;"><b>' . $customField[ 'title' ] . '</b></label>&nbsp;&nbsp;';
+									echo '<input type="checkbox" name="' . $this->prefix . $customField['name'] . '" id="' . $this->prefix . $customField['name'] . '" value="yes"';
+									if ( get_post_meta( $post->ID, $this->prefix . $customField['name'], true ) == "yes" )
+										echo ' checked="checked"';
+									echo '" style="width: auto;" />';
+									break;
+								}
+								case "textarea": {
+									// Text area
+									echo '<label for="' . $this->prefix . $customField[ 'name' ] .'"><b>' . $customField[ 'title' ] . '</b></label>';
+									echo '<textarea name="' . $this->prefix . $customField[ 'name' ] . '" id="' . $this->prefix . $customField[ 'name' ] . '" columns="30" rows="3">' . htmlspecialchars( get_post_meta( $post->ID, $this->prefix . $customField[ 'name' ], true ) ) . '</textarea>';
+									break;
+								}
+								default: {
+									// Plain text field
+									echo '<label for="' . $this->prefix . $customField[ 'name' ] .'"><b>' . $customField[ 'title' ] . '</b></label>';
+									echo '<input type="text" name="' . $this->prefix . $customField[ 'name' ] . '" id="' . $this->prefix . $customField[ 'name' ] . '" value="' . htmlspecialchars( get_post_meta( $post->ID, $this->prefix . $customField[ 'name' ], true ) ) . '" />';
+									break;
+								}
+							}
+							?>
+							<?php if ( $customField[ 'description' ] ) echo '<p>' . $customField[ 'description' ] . '</p>'; ?>
+					<?php
+					}
+				} ?>
+			</div>
+			<?php
+		}
+		
+				/**
+				* Save the new Custom Fields values
+				*/
+				function saveCustomFields( $post_id, $post ) {
+					if ( !wp_verify_nonce( $_POST[ 'wpfolio-custom-fields_wpnonce' ], 'wpfolio-custom-fields' ) )
+						return;
+					if ( !current_user_can( 'edit_post', $post_id ) )
+						return;
+					if ( $post->post_type != 'page' && $post->post_type != 'post' )
+						return;
+					foreach ( $this->customFields as $customField ) {
+						if ( current_user_can( $customField['capability'], $post_id ) ) {
+							if ( isset( $_POST[ $this->prefix . $customField['name'] ] ) && trim( $_POST[ $this->prefix . $customField['name'] ] ) ) {
+								update_post_meta( $post_id, $this->prefix . $customField[ 'name' ], $_POST[ $this->prefix . $customField['name'] ] );
+							} else {
+								delete_post_meta( $post_id, $this->prefix . $customField[ 'name' ] );
+							}
+						}
+					}
+				}
+
+			} // End Class
+
+		} // End if class exists statement
+
+		// Instantiate the class
+		if ( class_exists('WPFolioCustomFields') ) {
+			$WPFolioCustomFields_var = new WPFolioCustomFields();
+		}				
+//END CUSTOM FIELDS code. ...if it even works.		
+
+
 /* Sidebars */
 	
 	if ( function_exists('register_sidebar') )
@@ -355,6 +537,7 @@ $options = array (
 );
 //...END THEME OPTIONS...//
 
+
 function mytheme_add_admin() {
 	global $themename, $shortname, $options;
 	if ( $_GET['page'] == basename(__FILE__) )
@@ -393,8 +576,10 @@ global $themename, $shortname, $options;
 if ( $_REQUEST['saved'] ) echo '<div id="message" class="updated fade"><p><strong>'.$themename.' settings saved.</strong></p></div>';
     if ( $_REQUEST['reset'] ) echo '<div id="message" class="updated fade"><p><strong>'.$themename.' settings reset.</strong></p></div>';?>
 
+
+
 <!-- WPFolio Theme Interface -->
-<div class="wrap">
+<div class="wrap"> <!-- WHERE DOES THIS DIV GET CLOSED? -->
 
 <h2><?php echo $themename; ?> settings</h2>
 <form method="post">
@@ -469,6 +654,7 @@ add_action('admin_menu', 'mytheme_add_admin'); ?>
 		<ul>
 		<?php wp_get_archives("type=yearly&show_post_count=$c"); ?>
 		</ul>
+		
 <?php
 	}
 	echo $after_widget;
