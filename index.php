4198c97f77448e77b384ee2fcd424848b7c574ad
slambert <steve@visitsteve.com>
2010-05-06 18:53:24 -0400

    changing classes and organizing
    changed .posted to .post-bottom-title in index.php and then organized style.css a bit and removed duplicate classes and ids for .align... tags.

--- index.php
+++ index.php
@@ -9,25 +9,25 @@
 <!-- begin post -->    
 <?php 	if (! empty($display_stats) ) { 		get_stats(1); 		echo "<br />"; 	} 	else if (($posts & empty($display_stats)) ) : foreach ($posts as $post) : the_post(); ?>   
 
-<div class="entry <?php $cat = get_the_category(); $cat = $cat[0]; echo $cat->category_nicename; ?>">   
+	<div class="entry <?php $cat = get_the_category(); $cat = $cat[0]; echo $cat->category_nicename; ?>">   
 
-<!-- START PREVIOUS/NEXT BUTTONS --> 
+			<!-- START PREVIOUS/NEXT BUTTONS --> 
 
-<!--Removing Previous/Next until ticket 380 is fixed http://dev.eyebeam.org/projects/wpfolio/ticket/380 
-<div class="prevnext">
-<?php next_post_link('%link', 'newer', TRUE); ?> <?php previous_post_link('%link', 'older', TRUE); ?>
-</div> .prevnext -->
+			<!--Removing Previous/Next until ticket 380 is fixed http://dev.eyebeam.org/projects/wpfolio/ticket/380 
+			<div class="prevnext">
+			<?php next_post_link('%link', 'newer', TRUE); ?> <?php previous_post_link('%link', 'older', TRUE); ?>
+			</div> .prevnext -->
 
-<!-- END PREVIOUS/NEXT BUTTONS -->    
+			<!-- END PREVIOUS/NEXT BUTTONS -->    
 
-<?php the_content(); ?>   
-</div> <!-- .entry --> 		    
+	<?php the_content(); ?>   
+	</div> <!-- .entry --> 		    
 
-<div class="posted">   
+<div class="post-bottom-title">   
 <b><?php the_title(); ?></b>  | <a href="<?php the_permalink() ?>" title="Permalink"><?php the_time('Y') ?></a> | <?php the_category(', '); ?> <?php the_tags('| Tags: ',', ',''); ?>  | <?php comments_popup_link(__('Comments (0)'), __('Comments (1)'), __('Comments (%)'), __(''), __('')); ?>
 
 <?php edit_post_link('edit this', '<br /><br /><span class="edit-link">', '</span>'); ?> <!--USER EDIT LINK-->
-</div><!-- .posted -->
+</div><!-- .post-bottom-title -->
      
 <?php comments_template(); ?> 	  
 
