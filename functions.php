165d9462460e71f68b2d5946a6e4abbb61f0e1c2
slambert <steve@visitsteve.com>
2010-05-14 02:14:19 -0400

    added threaded comments function

--- functions.php
+++ functions.php
@@ -1,3 +1,14 @@
+<?php  // enable threaded comments
+function enable_threaded_comments(){
+	if (!is_admin()) {
+		if (is_singular() AND comments_open() AND (get_option('thread_comments') == 1))
+			wp_enqueue_script('comment-reply');
+		}
+}
+add_action('get_header', 'enable_threaded_comments');
+
+?>
+
 <?php 
 
 /* Sidebars */
