e0c3351a7dc347b00788cf7fb38e12c2ff282826
slambert <steve@visitsteve.com>
2010-05-26 11:21:59 -0400

    updating link in footer widget
    Now links to wpfolio.visitsteve.com

--- functions.php
+++ functions.php
@@ -225,7 +225,7 @@ function rss_credits_in_footer() {
 		if ( $show_comment_feed ){
 			echo   ' <a href="feed:' . get_bloginfo("comments_rss2_url") . '"><img src="' . get_bloginfo("template_url") . '/images/comment.gif" border="0" alt="Comments Feed" ></a> ' ;}
 		if ( $show_credits ){
-			echo ' &bull; <small>Credits: <a href="http://www.wordpress.org">Wordpress</a> | <a href="http://dev.eyebeam.org/projects/wpfolio/wiki/WPFolio">WPFolio</a> | <a href="http://eyebeam.org/">Eyebeam</a></small>' ;}
+			echo ' &bull; <small>Credits: <a href="http://www.wordpress.org">Wordpress</a> | <a href="http://wpfolio.visitsteve.com">WPFolio</a> | <a href="http://eyebeam.org/">Eyebeam</a></small>' ;}
 			
 			
 		/* After widget (defined by themes). */
