30d9b152cb8f6d1ffbabaa07f9b5d7ec04636038
slambert <steve@visitsteve.com>
2010-07-01 18:05:38 -0400

    first stab at consolidating & noticed other problems
    will return to this soon.

--- header.php
+++ header.php
@@ -37,72 +37,27 @@ http://wpfolio.visitsteve.com/wiki
 
  <style type="text/css" media="all">
  /* CSS inserted by theme options */
-	body { 
+	body, #content, .title, .nav, .widgettitle { 
 		font-family : <?php echo $WPFolio_body_font; ?>;
-		color: #<?php echo $WPFolio_body_color; ?>;}
+		}
+	body, .container, .container p, #content, div.notable-post {
+		color: #<?php echo $WPFolio_body_color; ?>;
+		}
 	h1,h2,h3,h4,h5,h6 {
 		font-family: <?php echo $WPFolio_headline_font; ?>;}
 	h1 { 
 		font-size: <?php echo $WPFolio_headline_size; ?>;}
-	h2,h3 {
+	h1,h2,h3, a:active, a:hover, .sf-menu li:hover, .sf-menu li.sfHover,
+.sf-menu a:focus, .sf-menu a:hover, .sf-menu a:active, div.notable-post h3 a, link, 	div.notable-post a:hover, #links, #links h1, #links h2, #links a:hover, div.prevnext a:hover {
 		color: #<?php $WPFolio_highlight_color; ?>;}
-	h4 {
+	h4, .sf-menu a, .sf-menu a:visited, div.notable-post a, #sidebar h2.widgettitle, #links ul, #links ul li, #links ul li ul, links a {
 		color: #<?php echo $WPFolio_second_color; ?>;}
 	.headertext {
 		visibility:<?php echo$WPFolio_visible; ?>; }
-	.headertext h1 a {
-		color: #<?php echo $WPFolio_highlight_color;?>; }
 	.container {
-		background-color: #<?php echo $WPFolio_foreground_color; ?>;
-		color: #<?php echo $WPFolio_body_color; ?>;}
-	.container p {
-		color: #<?php echo $WPFolio_body_color; ?>; }
-	a:active  {
-		color: #<?php echo $WPFolio_highlight_color; ?>; }
-	a:hover  {
-		color: #<?php echo $WPFolio_highlight_color; ?>; }
-	#content {
-		font-family : <?php echo $WPFolio_body_font; ?>; }
-	.title {
-		font-family : <?php echo $WPFolio_body_font; ?>;}
-	.nav, .widgettitle {
-		font-family : <?php echo $WPFolio_body_font; ?>; 
+		background-color: #<?php echo $WPFolio_foreground_color; ?>;}
+	.nav, .widgettitle, div.prevnext{
 		text-transform:<?php echo $WPFolio_text_transform ?>;}
-	.sf-menu a, .sf-menu a:visited {
-		color: #<?php echo $WPFolio_second_color; ?>;}
-	.sf-menu li:hover, .sf-menu li.sfHover,
-.sf-menu a:focus, .sf-menu a:hover, .sf-menu a:active {
-		color: #<?php echo $WPFolio_highlight_color; ?>;}
-	div.notable-post {
-		color: #<?php echo $WPFolio_body_color; ?>;}
-	div.notable-post h3 a, link {
-		color: #<?php echo $WPFolio_highlight_color; ?>;}
-	div.notable-post a {
-		color: #<?php echo $WPFolio_second_color; ?>;}
-	div.notable-post a:hover {
-		color: #<?php echo $WPFolio_highlight_color; ?>;}
-	#sidebar h2.widgettitle {
-		color: #<?php echo $WPFolio_second_color; ?>;}
-	#content {
-		color: #<?php echo $WPFolio_body_color; ?>; }
-	#links{
-		color: #<?php echo $WPFolio_highlight_color; ?>;}
-	#links h1, h2 {
-		color: #<?php echo $WPFolio_highlight_color; ?>;}
-	#links ul {
-		color: #<?php echo $WPFolio_second_color; ?>;}
-	#links ul li {
-		color: #<?php echo $WPFolio_second_color; ?>;}
-	#links ul li ul {
-		color: #<?php echo $WPFolio_second_color; ?>;}
-	#links a {
-		color: #<?php echo $WPFolio_second_color; ?>;}
-	#links a:hover {
-		color: #<?php echo $WPFolio_highlight_color; ?>;}
-	div.prevnext{
-		text-transform:<?php echo $WPFolio_text_transform ?>;}
-	div.prevnext a:hover {
-		background-color: #<?php echo $WPFolio_highlight_color; ?>;}
 	div.footer {
 	background-color: #<?php echo $WPFolio_foreground_color; ?>;
 	border-top: solid 1px #<?php echo $WPFolio_body_backgroundcolor; ?>;}
@@ -120,6 +75,13 @@ $(document).ready(function() {
 $('ul.sf-menu').superfish();
 });
 </script>
+
+<script language="javascript" type="text/javascript" src="<?php echo bloginfo('stylesheet_directory') ?>/js/hoverIntent.js"></script>
+<script language="javascript" type="text/javascript" src="<?php echo bloginfo('stylesheet_directory') ?>/js/superfish.js"></script>
+<script language="javascript" type="text/javascript" src="<?php echo bloginfo('stylesheet_directory') ?>/js/supersubs.js"></script>
+<script type="text/javascript" src="http://localhost:8888/wp/wp-content/themes/thematic/library/scripts/thematic-dropdowns.js"></script>
+
+
 <!-- end superfish -->
 
 <?php //for support of js threaded comments
