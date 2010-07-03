e66d1a4dab734c5e3b2298dafdc0c08b3c3da29b
slambert <steve@visitsteve.com>
2010-07-03 13:52:31 -0700

    superfish works again
    needed the call for jquery-1.2.6.min
    fixed shadows so style looks better

--- header.php
+++ header.php
@@ -68,16 +68,15 @@ http://wpfolio.visitsteve.com/wiki
 <!--
 Add if you want to enable the SuperFish Navbar. It will need styling! 
 <link rel="stylesheet" href="<?php bloginfo('stylesheet_directory'); ?>/css/superfish-navbar.css" type="text/css" media="screen"/> -->
-
+<script src="<?php bloginfo('stylesheet_directory'); ?>/js/jquery-1.2.6.min.js" type="text/javascript"></script>
+<script type="text/javascript" src="<?php bloginfo('stylesheet_directory'); ?>/js/hoverIntent.js"></script>
+<script src="<?php bloginfo('stylesheet_directory'); ?>/js/superfish.js" type="text/javascript"></script>
+<script language="javascript" type="text/javascript" src="<?php echo bloginfo('stylesheet_directory') ?>/js/supersubs.js"></script>
 <script type="text/javascript">
 $(document).ready(function() {
 $('ul.sf-menu').superfish();
 });
 </script>
-
-<script language="javascript" type="text/javascript" src="<?php echo bloginfo('stylesheet_directory') ?>/js/hoverIntent.js"></script>
-<script language="javascript" type="text/javascript" src="<?php echo bloginfo('stylesheet_directory') ?>/js/superfish.js"></script>
-<script language="javascript" type="text/javascript" src="<?php echo bloginfo('stylesheet_directory') ?>/js/supersubs.js"></script>
 
 <!-- end superfish -->
 
