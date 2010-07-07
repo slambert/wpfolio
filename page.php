c06173ed39889c065bf2284ebfe6755bb20aae7d
slambert <steve@visitsteve.com>
2010-07-06 22:26:11 -0700

    now valid and formatted better, resume style works
    correct number of divs open and close and h2 out of the link.

--- page.php
+++ page.php
@@ -10,24 +10,22 @@
 <!-- begin page -->    
 <?php 	if (! empty($display_stats) ) { 		get_stats(1); 		echo "<br />"; 	} 	else if (($posts & empty($display_stats)) ) : foreach ($posts as $post) : start_wp(); ?> 
 
-<div id="content">
-	<div class="page"> 
-<div class="<?php wp_title('',true,''); ?>">
+<div id="content"> 
+	<div class="<?php wp_title('',true,''); ?>">
 
-<a href="<?php the_permalink() ?>" title="Permalink"><?php if (is_page('home')) { echo ""; } else if (is_page("")) {
-	 echo the_title('<h2 class="pagetitle">','</h2>');
+		<h2 class="pagetitle"><a href="<?php the_permalink() ?>" title="Permalink"><?php if (is_page('home')) { echo ""; } else if (is_page("")) {
+	 echo the_title();
 	}
-?></a>
-</div><!--.page title--> 
+?></a></h2>
 
-<?php the_content(); ?>  
+		<?php the_content(); ?>  
 
-<?php wp_link_pages(); ?>
+		<?php wp_link_pages(); ?>
   
-<div class="post-bottom-title"> <?php comments_popup_link(__('Comments (0)'), __('Comments (1)'), __('Comments (%)'), __(''), __('')); ?>	</div>
+		<div class="post-bottom-title"> <?php comments_popup_link(__('Comments (0)'), __('Comments (1)'), __('Comments (%)'), __(''), __('')); ?>	</div><!-- .post-bottom-title -->
 <?php comments_template(); ?> 	 
 <?php edit_post_link('edit this', '<br /><br /><span class="edit-link">', '</span>'); ?> <!--USER EDIT LINK-->
-</div><!-- .page -->
+	</div><!-- .page[title] -->
 </div><!-- #content -->
    
 
