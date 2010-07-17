8916f3573f95a04777d8b3f74d471aba70cebbdb
Steve Lambert <steveMB@Steve-Lamberts-iMac.local>
2010-07-17 16:16:47 -0700

    more readable pages - new .widemargins and .justify class

--- page.php
+++ page.php
@@ -19,19 +19,25 @@
 ?></a></h2>
 		
 		<?php if (is_page(array('RESUME','Resume','resume','CV','cv'))) { ?>
-		<div class="cv-style">
+		<div class="cv-style widemargins">
 		<?php the_content(); ?> 
 		</div><!-- .cvstyle -->
 		
 		<? } else if (is_page()) { ?>
+		<div class="widemargins">
 		<?php the_content(); ?> 
+		
 		<? } ?>
 		 
 		<?php wp_link_pages(); ?>
   
 		<div class="post-bottom-title"> <?php comments_popup_link(__('Comments (0)'), __('Comments (1)'), __('Comments (%)'), __(''), __('')); ?>	</div><!-- .post-bottom-title -->
-<?php comments_template(); ?> 	 
-<?php edit_post_link('edit this', '<br /><br /><span class="edit-link">', '</span>'); ?> <!--USER EDIT LINK-->
+		
+		<div class="widemargins">
+		<?php comments_template(); ?> 	 
+		<?php edit_post_link('edit this', '<br /><br /><span class="edit-link">', '</span>'); ?> <!--USER EDIT LINK-->
+		</div><!--.widemargins-->
+
 	</div><!-- .page[title] -->
 </div><!-- #content -->
    
