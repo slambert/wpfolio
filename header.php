bf01a110efe45f4c9852067394f4d525bd29be16
Patrick Carey <patrickcar@gmail.com>
2010-10-18 13:19:29 -0400

    new functions to call the theme options, theme options CSS removed from head section in header.php

--- header.php
+++ header.php
@@ -22,35 +22,6 @@
 	<?php if (function_exists('is_tag')) { if ( is_tag() ) { ?><?php bloginfo('name'); ?>&nbsp;|&nbsp;<?php  single_tag_title("Tag Archive:", true); } } ?>
 	</title>  
 
-<style type="text/css" media="all">
- /* CSS inserted by theme options */
-	body, #content, .title, .nav, .widgettitle { 
-		font-family : <?php echo $WPFolio_body_font; ?>;
-		}
-	body, .container, .container p, #content, div.notable-post {
-		color: #<?php echo $WPFolio_body_color; ?>;
-		}
-	h1,h2,h3,h4,h5,h6 {
-		font-family: <?php echo $WPFolio_headline_font; ?>;}
-	h1 { 
-		font-size: <?php echo $WPFolio_headline_size; ?>;}
-	h1,h2,h3, .headertext h1 a, a:link, a:active, a:hover, .sf-menu li:hover, .sf-menu li.sfHover, .sf-menu a:focus, .sf-menu a:hover, .sf-menu a:active, div.notable-post h3 a, link, 	div.notable-post a:hover, #links, #links h1, #links h2, #links a:hover, div.prevnext a:hover {
-		color: #<?php echo $WPFolio_highlight_color; ?>; }
-	h4, .sf-menu a, .sf-menu a:visited, div.notable-post a, #sidebar h2.widgettitle, #links ul, #links ul li, #links ul li ul, links a {
-		color: #<?php echo $WPFolio_second_color; ?>;}
-	.headertext {
-		visibility:<?php echo$WPFolio_visible; ?>; }
-	.container, .sf-menu li a, .sf-menu li li, .sf-menu li li li{
-		background-color: #<?php echo $WPFolio_foreground_color; ?>;}
-	.nav, .widgettitle, div.prevnext{
-		text-transform:<?php echo $WPFolio_text_transform ?>;}
-	div.footer {
-	background-color: #<?php echo $WPFolio_foreground_color; ?>;
-	border-top: solid 1px #<?php echo get_theme_mod( 'background_color' ); ?>;}
-	.sf-menu li li, .sf-menu li li li {
-	border-top: solid 1px #<?php echo get_theme_mod( 'background_color' ); ?>;
-	border-left: solid 1px #<?php echo get_theme_mod( 'background_color' ); ?>;} 
- </style>
 
 <!-- Superfish Support -->
 
@@ -80,6 +51,7 @@ $('ul.sf-menu').superfish();
 <?php } else if (is_page("")) { } ?>
 
 <link href="<?php bloginfo('stylesheet_url'); ?>" rel="stylesheet" type="text/css" />
+<link href="<?php bloginfo('url');?>/?wp_folio_css=css" rel="stylesheet" type="text/css" />
 
 
 <!-- calling wp_head -->
