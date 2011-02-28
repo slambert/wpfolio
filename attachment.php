dc060a83d6865e180cc015fa19289e796f40f71c
laras126 <laras126@gmail.com>
2011-02-27 20:35:10 -0500

    should work but doesnt

--- attachment.php
+++ attachment.php
@@ -7,28 +7,31 @@
 
 <div id="content"><!-- BEGIN CONTENT-->
 	
-	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>	
 	
-	
-	
-		
-		<?php 
-		
-			#Print the large version of the attached image
-			
-		print wp_get_attachment_image($post->ID, 'large'); ?>
-	
-	
-	
-		<div class="post-bottom-title">   
-		
-		<strong><a href="<?php the_permalink() ?>" title="Permalink for <?php the_title_attribute(); ?>">
-		<?php the_title(); ?></a></strong>  | <?php the_time('Y') ?> | <?php the_category(', '); ?> <?php echo get_the_term_list($post->ID, 'medium', '| Media: ', ', ', ''); ?> <?php the_tags('| Tags: ',', ',''); ?>  <?php comments_popup_link(__('| Comments (0)'), __('| Comments (1)'), __('| Comments (%)'), __(''), __('')); ?>
+	<?php 
+	#Print the large version of the attached image	
+	print wp_get_attachment_image($post->ID, 'large'); ?>
 
+	<div class="post-bottom-title">   
+
+		<div class="next">
+			<a href="<?php previous_image_link(); ?>" rel="prev">Later</a>
+		</div>
+		<div class="prev">
+			<a href="<?php next_image_link(); ?>" rel="next">Earlier</a></div> <!-- flipped on purpose -->
+		</div> <!--.prevnext --> 		
+		
+		<strong><a href="<?php the_permalink() ?>" title="Permalink for <?php the_title_attribute(); ?>"><?php the_title(); ?></a></strong>  | <?php the_time('Y') ?> | <?php the_category(', '); ?> <?php echo get_the_term_list($post->ID, 'medium', '| Media: ', ', ', ''); ?> <?php the_tags('| Tags: ',', ',''); ?>  <?php comments_popup_link(__('| Comments (0)'), __('| Comments (1)'), __('| Comments (%)'), __(''), __('')); ?>
+	
 		<?php edit_post_link('edit this', '<br /><br /><span class="edit-link">', '</span>'); ?> <!--USER EDIT LINK-->
 		
-		</div><!-- .post-bottom-title -->
+		
+
+		
 	
+	</div><!-- .post-bottom-title -->
+
 	
 	<?php endwhile; else: ?>
 	<p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
--- single.php
+++ single.php
@@ -43,7 +43,6 @@
 
 		<div id="sidebar">
 			<?php if ( function_exists('dynamic_sidebar') && dynamic_sidebar('sidebar') ) ; ?> 
-
 		</div><!-- #sidebar -->
 
 	</div><!-- #content -->
