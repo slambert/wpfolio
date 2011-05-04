601369afd285f9f0f71544fa5fd3ad41f4f4650b
laras126 <laras126@gmail.com>
2011-05-04 19:37:24 -0400

    PHP version conditional for wiki feed function
    fixed comments and <!
    
     Author:    laras126 <laras126@gmail.com>
     modified:   functions.php
    rebased Laras changes. will prevent a total crash if someone is using an outdated version of php.

--- functions.php
+++ functions.php
@@ -145,47 +145,58 @@ function wpfolio_admin_footer() {
 	echo 'Thank you for creating with <a href="http://wordpress.org/" target="_blank">WordPress</a>. | <a href="http://codex.wordpress.org/" target="_blank">Documentation</a> | <a href="http://wordpress.org/support/forum/4" target="_blank">Feedback</a> | <a href="http://wpfolio.visitsteve.com/">Theme by WPFolio</a>';
 } 
 add_filter('admin_footer_text', 'wpfolio_admin_footer');
-
-
-
-// Add WPFolio Wiki site as a Dashboard Feed 
-// Thanks to bavotasan.com: http://bavotasan.com/tutorials/display-rss-feed-with-php/ 
-
-function custom_dashboard_widget() {
-
-	$rss = new DOMDocument();
-	$rss->load('http://wpfolio.visitsteve.com/wiki/feed');
-	$feed = array();
-	foreach ($rss->getElementsByTagName('item') as $node) {
-		$item = array ( 
-			'title' => $node->getElementsByTagName('title')->item(0)->nodeValue,
-			// 'desc' => $node->getElementsByTagName('description')->item(0)->nodeValue,
-			'link' => $node->getElementsByTagName('link')->item(0)->nodeValue,
-			'date' => $node->getElementsByTagName('pubDate')->item(0)->nodeValue,
-			);
-		array_push($feed, $item);
-	}
-	$limit = 5; // change how many posts to display here
-	echo '<ul>'; // wrap in a ul
-	for($x=0;$x<$limit;$x++) {
-		$title = str_replace(' & ', ' &amp; ', $feed[$x]['title']);
-		$link = $feed[$x]['link'];
-		// $description = $feed[$x]['desc'];
-		$date = date('l F d, Y', strtotime($feed[$x]['date']));
-		echo '<li><p><strong><a href="'.$link.'" title="'.$title.'">'.$title.'</a></strong>';
-		echo ' - '.$date.'</p></li>';
-		// echo '<p>'.$description.'</p>';
-	}
-	echo '</ul>';
-	echo '<p class="textright"><a href="http://wpfolio.visitsteve.com/wiki/category/news" class="button">View all</a></p>'; // link to site
 	
-}
+// Testing to see if the PHP version is up to date. If it is, add a WPFolio RSS feed widget, and if it's not, add a widget prompting an upgrade.
+if (version_compare(PHP_VERSION, '5.3.0', '>=')) {
+
+	// Add WPFolio Wiki site as a Dashboard Feed 
+	// Thanks to bavotasan.com: http://bavotasan.com/tutorials/display-rss-feed-with-php/ 
 	
-function add_custom_dashboard_widget() {
-	wp_add_dashboard_widget('custom_dashboard_widget', 'WPFolio News', 'custom_dashboard_widget');
+	function custom_dashboard_widget() {
+		$rss = new DOMDocument();
+		$rss->load('http://wpfolio.visitsteve.com/wiki/feed');
+		$feed = array();
+		foreach ($rss->getElementsByTagName('item') as $node) {
+			$item = array ( 
+				'title' => $node->getElementsByTagName('title')->item(0)->nodeValue,
+				// 'desc' => $node->getElementsByTagName('description')->item(0)->nodeValue,
+				'link' => $node->getElementsByTagName('link')->item(0)->nodeValue,
+				'date' => $node->getElementsByTagName('pubDate')->item(0)->nodeValue,
+				);
+			array_push($feed, $item);
+		}
+		$limit = 5; // change how many posts to display here
+		echo '<ul>'; // wrap in a ul
+		for($x=0;$x<$limit;$x++) {
+			$title = str_replace(' & ', ' &amp; ', $feed[$x]['title']);
+			$link = $feed[$x]['link'];
+			// $description = $feed[$x]['desc'];
+			$date = date('l F d, Y', strtotime($feed[$x]['date']));
+			echo '<li><p><strong><a href="'.$link.'" title="'.$title.'">'.$title.'</a></strong>';
+			echo ' - '.$date.'</p></li>';
+			// echo '<p>'.$description.'</p>';
+		}
+		echo '</ul>';
+		echo '<p class="textright"><a href="http://wpfolio.visitsteve.com/wiki/category/news" class="button">View all</a></p>'; // link to site	
+	}
+	
+	function add_custom_dashboard_widget() {
+		wp_add_dashboard_widget('custom_dashboard_widget', 'WPFolio News', 'custom_dashboard_widget');
+	}
+	add_action('wp_dashboard_setup', 'add_custom_dashboard_widget');
+
+} else {
+
+	function print_php_error() {
+	$error = "<p style='color:red; font-size: 1.5em;'>You are using an outdated version of PHP.  WordPress doesn't support it and neither does WPFolio!  Upgrade to the latest version of PHP.</p>";
+		echo $error;
+	}
+	
+	function add_error_widget() {
+		wp_add_dashboard_widget('error_widget', 'IMPORTANT!', 'print_php_error');	
+	} 
+	
+	add_action('wp_dashboard_setup', 'add_error_widget' );
 }
-add_action('wp_dashboard_setup', 'add_custom_dashboard_widget');
-
-
 
 ?>
